722621f797a606fb1e7e590430967839794c7204
Andrew Grosser <dioptre@gmail.com>
2020-05-06 02:29:54 -0700

    fixed missing config

--- WuHostNull.cpp
+++ WuHostNull.cpp
@@ -1,5 +1,11 @@
 #include "WuHost.h"
 
+struct WuConf {
+  const char* host = "127.0.0.1";
+  const char* port = "9555";
+  int maxClients = 256;
+};
+
 WuHost* WuHostCreate(const WuConf*) { return NULL; }
 int32_t WuHostServe(WuHost*, WuEvent*) { return 0; }
 void WuHostRemoveClient(WuHost*, WuClient*) {}
